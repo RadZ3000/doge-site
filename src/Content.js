3b5195b95738101202d40cd45f293da5066c4396
RadZ3000 <86256395+RadZ3000@users.noreply.github.com>
2021-12-07 08:08:10 -0600

    latest

--- src/Content.js
+++ src/Content.js
@@ -30,24 +30,28 @@ function Content() {
         <button className="Button" id="button-3">OUR TWITTER</button>
       </div>
 
-      <div className="Features">
-        <h2>Project Features</h2>
-        <p>{featuresConst}</p>
-      </div>
+      <div id="Content">
 
-      <div className="Tokenomics">
-        <h2 id='tokenomics'>Tokenomics</h2>
-        <p>{tokenomicsConst}</p>
-      </div>
+        <div className="Features">
+          <h2>PROJECT FEATURES</h2>
+          <p>{featuresConst}</p>
+        </div>
 
-      <div className="Roadmap">
-        <h2 id='roadmap'>Roadmap</h2>
-        <p>{roadmapConst}</p>
-      </div>
+        <div className="Tokenomics">
+          <h2 id='tokenomics'>TOKENOMICS</h2>
+          <p>{tokenomicsConst}</p>
+        </div>
 
-      <div className="Info">
-        <h2 id="token-info">Token Info</h2>
-        <p>{tokenInfoConst}</p>
+        <div className="Roadmap">
+          <h2 id='roadmap'>ROADMAP</h2>
+          <p>{roadmapConst}</p>
+        </div>
+
+        <div className="Info">
+          <h2 id="token-info">TOKEN INFO</h2>
+          <p>{tokenInfoConst}</p>
+        </div>
+
       </div>
 
     </div>
